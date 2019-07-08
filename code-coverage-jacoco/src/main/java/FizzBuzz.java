c04f1628234eac3c38007bc3e00a462c2e339aea
Gourav Shah <gs@initcron.org>
2019-07-08 12:13:02 +0530

    testing pass

--- code-coverage-jacoco/src/main/java/FizzBuzz.java
+++ code-coverage-jacoco/src/main/java/FizzBuzz.java
@@ -5,6 +5,7 @@ public class FizzBuzz {
 
     public String getFizzBuzzWord(int number) {
         StringBuilder fizzBuzzWord = new StringBuilder();
+        public static final String PASSWORD = "password";
 
         if (number % 3 == 0) {
             fizzBuzzWord.append("Fizz");
